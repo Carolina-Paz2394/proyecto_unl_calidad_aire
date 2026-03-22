8a6cba706622fcfd5c13fd977c87ce8889501183
Carolina-Paz2394 <carolina.paz@unl.edu.ec>
2026-03-22 00:31:59 -0500

    Delete macros directory

--- macros/clasificacion_aire.sql
+++
@@ -1,7 +0,0 @@
-{% macro clasificacion_aire(indice) %}
-case
-    when {{ indice }} <= 50 then 'Bueno'
-    when {{ indice }} > 50 and {{ indice }} <= 100 then 'Moderado'
-    else 'Dañino'
-end
-{% endmacro %}
