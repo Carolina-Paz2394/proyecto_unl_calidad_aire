7f8f35d24e78b8f215f1eb8dc964a28cd4418b8d
Carolina-Paz2394 <carolina.paz@unl.edu.ec>
2026-03-22 00:32:17 -0500

    Delete target directory

--- target/compiled/proyecto_unl/models/staging/stg_calidad_aire.sql
+++
@@ -1,7 +0,0 @@
-
-
-select
-    ciudad_id as nombre_ciudad,
-    indice_ica,
-    particulas_pm25
-from "db_warehouse"."public"."calidad_aire"
--- target/compiled/proyecto_unl/models/staging/stg_clima.sql
+++
@@ -1,7 +0,0 @@
-
-
-select
-    ciudad as nombre_ciudad,
-    temperatura as temp_celsius,
-    condicion as condicion_climatica
-from "db_warehouse"."public"."datos_climaticos"
