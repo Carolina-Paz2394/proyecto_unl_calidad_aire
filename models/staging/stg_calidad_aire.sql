fc22e4ae8ac80f20b76d1f8dff3a5f9bc8121694
Carolina-Paz2394 <carolina.paz@unl.edu.ec>
2026-03-22 00:30:24 -0500

    Delete models directory

--- models/staging/stg_calidad_aire.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    ciudad_id as nombre_ciudad,
-    indice_ica,
-    particulas_pm25
-from {{ source('semillas', 'calidad_aire') }}
--- models/staging/stg_clima.sql
+++
@@ -1,7 +0,0 @@
-{{ config(materialized='view') }}
-
-select
-    ciudad as nombre_ciudad,
-    temperatura as temp_celsius,
-    condicion as condicion_climatica
-from {{ source('semillas', 'datos_climaticos') }}
